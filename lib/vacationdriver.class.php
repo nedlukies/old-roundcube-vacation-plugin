09d645c8bea3817e2bf2cec92bb501a1cfd4c696
André Oliveira <me@andreoliveira.io>
2018-11-13 16:40:53 -0500

    Forced UTF-8 for Subject and Body. Started fixing format.

--- lib/vacationdriver.class.php
+++ lib/vacationdriver.class.php
@@ -2,137 +2,148 @@
 /*
  * VacationDriver base class
  *
- * @package	plugins
- * @uses	rcube_plugin
- * @author	Jasper Slits <jaspersl at gmail dot com>
- * @version	1.9
+ * @package    plugins
+ * @uses    rcube_plugin
+ * @author    Jasper Slits <jaspersl at gmail dot com>
+ * @version    1.9
  * @license     GPL
- * @link	https://sourceforge.net/projects/rcubevacation/
- * @todo	See README.TXT
+ * @link    https://sourceforge.net/projects/rcubevacation/
+ * @todo    See README.TXT
  */
 
-abstract class VacationDriver {
-	protected $cfg,$dotforward = array();
-	protected $rcmail,$user,$forward,$body,$subject,$aliases = "", $activefrom, $activeuntil;
-	protected $enable,$keepcopy = false;
+abstract class VacationDriver
+{
+    protected $cfg, $dotforward = array();
+    protected $rcmail, $user, $forward, $body, $subject, $aliases = "", $activefrom, $activeuntil;
+    protected $enable, $keepcopy = false;
 
-	abstract public function _get();
-	abstract public function init();
-	abstract protected function setVacation();
+    abstract public function _get();
+    abstract public function init();
+    abstract protected function setVacation();
 
-	// Provide easy access for the drivers to frequently used objects
-	public function __construct() {
-		$this->rcmail = rcmail::get_instance();
-		$this->user = $this->rcmail->user;
-		$this->identity = $this->user->get_identity();
-		$child_class = strtolower( get_class($this));
-	}
+    // Provide easy access for the drivers to frequently used objects
+    public function __construct()
+    {
+        $this->rcmail = rcmail::get_instance();
+        $this->user = $this->rcmail->user;
+        $this->identity = $this->user->get_identity();
+        $child_class = strtolower(get_class($this));
+    }
 
         /*
         
-         */
-	public final function setIniConfig(array $inicfg)
-	{
-		$this->cfg = $inicfg;
-	}
+     */
+    public final function setIniConfig(array $inicfg)
+    {
+        $this->cfg = $inicfg;
+    }
 
 
-	public final function setDotForwardConfig($child_class,$config) {
-		// forward settings are shared by ftp,sshftp and setuid driver. 
-		if (in_array($child_class ,array('ftp','sshftp','setuid'))) {
-			$this->dotforward = $config;
-		}
-	}
+    public final function setDotForwardConfig($child_class, $config)
+    {
+        // forward settings are shared by ftp,sshftp and setuid driver. 
+        if (in_array($child_class, array('ftp', 'sshftp', 'setuid'))) {
+            $this->dotforward = $config;
+        }
+    }
 
-	// Helper method for the template to determine if user is allowed to enter aliases
-	public final function useAliases()
-	{
+    // Helper method for the template to determine if user is allowed to enter aliases
+    public final function useAliases()
+    {
 
-		return (isset($this->dotforward['alias_identities']) && $this->dotforward['alias_identities']);
-	}
+        return (isset($this->dotforward['alias_identities']) && $this->dotforward['alias_identities']);
+    }
 
-public function loadDefaults() {
+    public function loadDefaults()
+    {
     // Load default subject and body.
 
-    if (empty($this->cfg['body'])) return false;
+        if (empty($this->cfg['body'])) return false;
 
 
-    $file = "plugins/vacation/" . $this->cfg['body'];
+        $file = "plugins/vacation/" . $this->cfg['body'];
+
+
+        if (is_readable($file)) {
+            $defaults = array('subject' => $this->cfg['subject']);
+            $defaults['body'] = file_get_contents($file);
+            return $defaults;
+        } else {
+            raise_error(array(
+                'code' => 601, 'type' => 'php', 'file' => __FILE__,
+                'message' => sprintf("Vacation plugin: s cannot be opened", $file)
+            ), true, true);
+        }
+    }
+    
     
 
-    if (is_readable($file)) {
-        $defaults = array('subject'=>$this->cfg['subject']);
-        $defaults['body'] = file_get_contents($file);
-        return $defaults;
-    } else {
-        raise_error(array('code' => 601, 'type' => 'php', 'file' => __FILE__,
-                    'message' => sprintf("Vacation plugin: s cannot be opened", $file)
-                ), true, true);
+    // This method will be used from vacation.js as an JSON/Ajax call or directly
+    public final function vacation_aliases($method = null)
+    {
+        $aliases = "";
+        $identities = $this->user->list_identities();
+        // Strip off the default identity, no need to alias that.
+        array_shift($identities);
+
+
+
+        foreach ($identities as $identity) {
+            // Strip domainname off. /usr/bin/vacation only deals with system users
+
+            $aliases .= array_shift(explode("@", $identity['email'])) . ",";
+        }
+
+        $str = substr($aliases, 0, -1);
+
+        // We use this method in both ftp.class.php and as Ajax callback
+
+        if (empty($identities)) {
+            // No identities found.
+//            $str = "To use aliases, add more identities.";
+        }
+
+        if ($method != null) {
+            return $str;
+        } else {
+            // Calls the alias_callback as defined in vacation.js
+            $this->rcmail->output->command('plugin.alias_callback', array('aliases' => $str));
+        }
     }
-}
-	
-	
 
-	// This method will be used from vacation.js as an JSON/Ajax call or directly
-	public final function vacation_aliases($method=null)
-	{
-		$aliases = "";
-		$identities = $this->user->list_identities();
-		// Strip off the default identity, no need to alias that.
-		array_shift($identities);
-	  
+
+    /*
+     * @return boolean True on succes, false on failure
+     */
+    final public function save()
+    {
+        $this->enable = ((null != rcube_utils::get_input_value('_vacation_enabled', rcube_utils::INPUT_POST)) ? '1' : '0');
+        $this->subject = rcube_utils::get_input_value('_vacation_subject', rcube_utils::INPUT_POST, true);
+        $this->activefrom = rcube_utils::get_input_value('_vacation_activefrom', rcube_utils::INPUT_POST);
+        $this->activeuntil = rcube_utils::get_input_value('_vacation_activeuntil', rcube_utils::INPUT_POST);
+        $this->body = rcube_utils::get_input_value('_vacation_body', rcube_utils::INPUT_POST, true);
+        $this->keepcopy = (null != rcube_utils::get_input_value('_vacation_keepcopy', rcube_utils::INPUT_POST));
+        $this->forward = rcube_utils::get_input_value('_vacation_forward', rcube_utils::INPUT_POST);
+        $this->aliases = rcube_utils::get_input_value('_vacation_aliases', rcube_utils::INPUT_POST);
+
+        // This method performs the actual work
+        return $this->setVacation();
 		
+    }
+    final public function getActionText()
+    {
+        if ($this->enable && empty($this->forward)) {
+            return "enabled_and_no_forward";
+        };
+        if ($this->enable && !empty($this->forward)) {
+            return "enabled_and_forward";
+        };
+        if (!$this->enable && !empty($this->forward)) {
+            return "disabled_and_forward";
+        };
+        if (!$this->enable && empty($this->forward)) {
+            return "disabled_and_no_forward";
+        };
 
-		foreach($identities as $identity)
-		{
-			// Strip domainname off. /usr/bin/vacation only deals with system users
-			
-			$aliases.=array_shift(explode("@",$identity['email'])).",";
-		}
-
-		$str = substr($aliases,0,-1);
-
-		// We use this method in both ftp.class.php and as Ajax callback
-
-		if (empty($identities))
-		{
-			// No identities found.
-//			$str = "To use aliases, add more identities.";
-		}
-
-		if ($method != null)
-		{
-			return $str;
-		} else {
-			// Calls the alias_callback as defined in vacation.js
-			$this->rcmail->output->command('plugin.alias_callback',  array('aliases'=>$str));
-		}
-	}
-
-
-	/*
-	 * @return boolean True on succes, false on failure
-	 */
-	final public function save() {
-		$this->enable = ((NULL != rcube_utils::get_input_value('_vacation_enabled', rcube_utils::INPUT_POST)) ? '1' : '0');
-		$this->subject = rcube_utils::get_input_value('_vacation_subject', rcube_utils::INPUT_POST);
-		$this->activefrom = rcube_utils::get_input_value('_vacation_activefrom', rcube_utils::INPUT_POST);
-		$this->activeuntil = rcube_utils::get_input_value('_vacation_activeuntil', rcube_utils::INPUT_POST);
-		$this->body = rcube_utils::get_input_value('_vacation_body', rcube_utils::INPUT_POST);
-		$this->keepcopy = (NULL != rcube_utils::get_input_value('_vacation_keepcopy', rcube_utils::INPUT_POST));
-		$this->forward = rcube_utils::get_input_value('_vacation_forward', rcube_utils::INPUT_POST);
-		$this->aliases = rcube_utils::get_input_value('_vacation_aliases', rcube_utils::INPUT_POST);
-
-		// This method performs the actual work
-		return $this->setVacation();
-	}
-
-	final public function getActionText()
-	{
-		if ($this->enable && empty($this->forward)) { return "enabled_and_no_forward"; };
-		if ($this->enable && !empty($this->forward)) {  return "enabled_and_forward"; };
-		if (! $this->enable && !empty($this->forward)) { return "disabled_and_forward"; };
-		if (! $this->enable && empty($this->forward)) { return "disabled_and_no_forward"; };
-		
-	}
-}?>
+    }
+} ?>
--- lib/vacationfactory.class.php
+++ lib/vacationfactory.class.php
@@ -1,37 +1,68 @@
 <?php
-/*
+/**
  * VacationFactory class
+ * 
+ * PHP version 7
  *
- * @package	plugins
- * @uses	rcube_plugin
- * @author	Jasper Slits <jaspersl at gmail dot com>
- * @version	1.9
- * @license     GPL
- * @link	https://sourceforge.net/projects/rcubevacation/
- * @todo	See README.TXT
+ * @category Class
+ * @package  Plugins
+ * @uses     rcube_plugin
+ * @author   Jasper Slits <jaspersl@gmail.com>
+ * @license  http://opensource.org/licenses/gpl-3.0 GNU/GPLv3
+ * @link     https://github.com/yuusou/roundcube-vacation-plugin
+ * @todo     See README.TXT
  */
 
-class VacationDriverFactory {
-	
-	public function __construct()
-	{
-		die("Cannot instantiate this class");		
-	}
+/**
+ * VacationFactory class
+ *
+ * @category Class
+ * @package  Plugins
+ * @uses     rcube_plugin
+ * @author   Jasper Slits <jaspersl@gmail.com>
+ * @author   Roman Plessl <roman@plessl.info>
+ * @author   Andre Oliveira <me@andreoliveira.io>
+ * @license  http://opensource.org/licenses/gpl-3.0 GNU/GPLv3
+ * @version  Release: 2.1
+ * @link     https://github.com/yuusou/roundcube-vacation-plugin
+ * @todo     See README.TXT
+ */
+class VacationDriverFactory
+{
 
-	/*
-	 * @param string driver class to be loaded
-	 * @return object specific driver */
-    public static function Create( $driver ) {
+    /**
+     * Class constructor
+     */
+    public function __construct()
+    {
+        die("Cannot instantiate this class");
+    }
+
+    /**
+     * Create driver.
+     * 
+     * @param string $driver class to be loaded
+     * 
+     * @return object specific driver 
+     */
+    public static function create(string $driver)
+    {
         $driver = strtolower($driver);
-		$driverclass = sprintf("plugins/vacation/lib/%s.class.php",$driver);
-		
-        if (! is_readable($driverclass)) {
-             rcube::raise_error(array('code' => 601,'type' => 'php','file' => __FILE__,
-                'message' => sprintf("Vacation plugin: Driver '%s' cannot be loaded using %s",$driver,$driverclass)
-                ),true, true);
+        $driverClass = sprintf("plugins/vacation/lib/%s.class.php", $driver);
+
+        if (!is_readable($driverClass)) {
+            rcube::raise_error(
+                array(
+                    'code' => 601, 'type' => 'php', 'file' => __FILE__,
+                    'message' => sprintf(
+                        "Vacation plugin: Driver '%s' cannot be loaded using %s", 
+                        $driver, $driverClass
+                    )
+                ), true, true
+            );
         }
-        
-		require $driverclass;
+
+        include $driverClass;
         return new $driver;
     }
-}?>
+} ?>
